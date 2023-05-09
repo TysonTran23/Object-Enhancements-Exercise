35eebbdbfa8b57f965b560069ad2c34b3821cbb1
Tyson Tran <tysontran@Tysons-MBP.hitronhub.home>
2023-05-09 11:13:52 -0700

    finished exercise

---
+++ app.js
@@ -0,0 +1,65 @@
+function createInstructor(firstName, lastName) {
+  return {
+    firstName: firstName,
+    lastName: lastName,
+  };
+}
+
+/* Write an ES2015 Version */
+const createInstructor = (firstName, lastName) => {
+  return {
+    firstName,
+    lastName,
+  };
+};
+
+var favoriteNumber = 42;
+
+var instructor = {
+  firstName: "Colt",
+};
+
+instructor[favoriteNumber] = "That is my favorite!";
+/* Write an ES2015 Version */
+let favoriteNumber = 42;
+const instructor = {
+  firstName: "colt",
+  [favoriteNumber]: "That is my favorite",
+};
+
+var instructor = {
+  firstName: "Colt",
+  sayHi: function () {
+    return "Hi!";
+  },
+  sayBye: function () {
+    return this.firstName + " says bye!";
+  },
+};
+/* Write an ES2015 Version */
+const instructor = {
+  firstName: "Colt",
+  sayHi() {
+    return "Hi!";
+  },
+  sayBye() {
+    return this.firstName + " says bye!";
+  },
+};
+
+const d = createAnimal("dog", "bark", "Woooof!");
+// {species: "dog", bark: ƒ}
+d.bark(); //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa");
+// {species: "sheep", bleet: ƒ}
+s.bleet(); //"BAAAAaaaa"
+
+const animal = (species, verb, noise) => {
+  return {
+    species,
+    [verb]() {
+      return noise;
+    },
+  };
+};
